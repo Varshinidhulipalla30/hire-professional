76a1be12f7871024d03c76c09cb3c77151dbd715
Varshinidhulipalla30 <dhulipallavarshini0730@gmail.com>
2025-11-30 09:16:01 +0530

    Updates

--- src/data/mock.js
+++
@@ -1,5 +0,0 @@
-export const professionals = [
-  { id: 1, name: "Rahul Singh", skill: "Electrician" },
-  { id: 2, name: "Sneha Jain", skill: "Cleaner" },
-  { id: 3, name: "Amit Kumar", skill: "Plumber" }
-];
